21ea6b5414f75a5153dbaa349692edf4369dc4da
Wessam Khaled <94244278+WessamKhaled@users.noreply.github.com>
2024-11-06 21:47:10 +0200

    splash view body

--- lib/Features/splash/presentation/views/splash_view.dart
+++ lib/Features/splash/presentation/views/splash_view.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/splash_view_body.dart';
+
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(title:const Text("AppBar"),),
+      body: SplashViewBody(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
 
 import 'Features/splash/presentation/views/splash_view.dart';
 
 
 void main() {
-  runApp(BooklyApp());
+  runApp(const BooklyApp());
 }
 class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
 
       home: SplashView(),
     );
