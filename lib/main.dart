7890607c7785abd4fd0d7fa7d0415961eeab7339
Wessam Khaled <94244278+WessamKhaled@users.noreply.github.com>
2024-11-07 22:21:04 +0200

    splash view body and change theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
+import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_connect/http/src/utils/utils.dart';
 
 import 'Features/splash/presentation/views/splash_view.dart';
 
-
 void main() {
   runApp(const BooklyApp());
 }
+
 class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
       debugShowCheckedModeBanner: false,
-
-      home: SplashView(),
+      home: const SplashView(),
     );
   }
 }
